e8a803516d7bb64bfcdb5f704380db00846ae87c
봄 <saebom@Saebomui-MacBookPro.local>
2025-02-18 21:36:15 +0900

    feat: 라우터 설정

--- routes.jsx
+++ routes.jsx
@@ -1,7 +1,26 @@
 import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
+const Layout = lazy(() => import("@components/Layout"));
+
+const HomePage = lazy(() => import("@pages/HomePage"));
+const AboutPage = lazy(() => import("@pages/AboutPage"));
+const ProjectPage = lazy(() => import("@pages/ProjectPage"));
+const ContactPage = lazy(() => import("@pages/ContactPage"));
+const ApplyPage = lazy(() => import("@pages/ApplyPage"));
+
 const router = createBrowserRouter([
-  { path: "/", element: <Layout /> },
-  { path: "/about", element: <AboutPage /> },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "/about", element: <AboutPage /> },
+      { path: "/project", element: <ProjectPage /> },
+      { path: "/contact", element: <ContactPage /> },
+      { path: "/apply", element: <ApplyPage /> },
+    ],
+  },
 ]);
+
+export default router;
