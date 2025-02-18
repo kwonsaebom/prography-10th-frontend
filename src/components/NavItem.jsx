fd74e131a115bd00b23f70f7311fb81eb1c4a277
봄 <saebom@Saebomui-MacBookPro.local>
2025-02-18 23:11:51 +0900

    feat: 헤더 설정 (네비게이션)

---
+++ src/components/NavItem.jsx
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router-dom";
+import PropTypes from "prop-types";
+
+NavItem.propTypes = {
+  to: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
+};
+export default function NavItem({ to, children }) {
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) => (isActive ? "text-primary" : undefined)}
+    >
+      {children}
+    </NavLink>
+  );
+}
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+import logo from "/images/logo_prography.png";
+import NavItem from "@components/NavItem";
+
 export default function Header() {
   return (
-    <>
-      <h1 className="bg-amber-50">header</h1>
-    </>
+    <header className="h-20 flex items-center px-20 justify-between">
+      <img src={logo} className="h-12" />
+      <nav className="h-12 text-white flex items-center gap-6">
+        <NavItem to="/about">About</NavItem>
+        <NavItem to="/project">Project</NavItem>
+        <NavItem to="/contact">Contact</NavItem>
+        <Link
+          to="apply"
+          className="bg-primary px-6 py-3 rounded-full text-white font-bold"
+        >
+          11기 지원하기
+        </Link>
+      </nav>
+    </header>
   );
 }
